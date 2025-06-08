601d1c23702079f0e2873d578111c616b0872331
nvp85 <61716664+nvp85@users.noreply.github.com>
2025-06-07 23:02:04 -0500

    complete the exercises

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,10 +2,20 @@ package org.example;
 
 public class Calculator {
     // Add a static function called add that takes two int parameters and returns their sum (as an int)
-
+    static int add(int a, int b) {
+        return a + b;
+    }
     // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
-
+    static int subtract(int a, int b) {
+        return a - b;
+    }
     // Add a static function called multiply that takes two int parameters and returns their product (as an int)
+    static int multiply(int a, int b) {
+        return a * b;
+    }
 
+    static int divide(int a, int b) {
+        return a / b;
+    }
     // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
 }
--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -5,12 +5,21 @@ public class StringUtilities {
 
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
-
+    static boolean shortString(String str) {
+        return str.length() < 5;
+    }
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
-
+    static char firstLetter(String str) {
+        return str.charAt(0);
+    }
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
-
+    static String censorAsparagus(String str) {
+        return str.replace("asparagus", "****");
+    }
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+    static String bigger(String str1, String str2) {
+        return str1.length() >= str2.length() ? str1 : str2;
+    }
 }
